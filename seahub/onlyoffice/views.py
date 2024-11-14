ed803e39ba086ca72402255fdfe5c896a87ed2a4
lovehunter9 <wangrx07@aliyun.com>
2024-11-14 15:12:14 +0800

    try to link to onlyoffice

--- seahub/onlyoffice/views.py
+++ seahub/onlyoffice/views.py
@@ -164,7 +164,8 @@ def onlyoffice_editor_callback(request):
         data = {'target_file': file_path}
 
         # update file
-        update_url = gen_inner_file_upload_url('update-api', update_token)
+        # update_url = gen_inner_file_upload_url('update-api', update_token)
+        update_url = gen_inner_file_upload_url('update-blks-api', update_token)
         resp = requests.post(update_url, files=files, data=data)
         if resp.status_code != 200:
             logger.error('update_url: {}'.format(update_url))
